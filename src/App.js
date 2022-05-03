46cd5f5a52a8c6336d42081206d11bb69514875e
owentr1369 <tamvaa1306@gmail.com>
2022-05-03 21:41:45 +0700

    build base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+// import Navigation from "./components/navigations";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navigation /> */}
+      <h1>Profile</h1>
+    </div>
+  );
+}
+
+export default App;
